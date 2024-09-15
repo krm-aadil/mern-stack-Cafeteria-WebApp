dd92615eddb97bd6a87570f911b1c922150e8e88
Aadil <CB010188students.apiit.lk>
2024-09-15 13:50:19 +0530

    adding admin role

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' } // Add role field with default 'user'
 });
 
 // Hash the password before saving the user
